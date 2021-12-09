08650d918a115c1a92a0b96edea6163cbb453fe8
zhaozengxing <961854268@qq.com>
2021-12-09 15:20:36 +0800

    2021/12/9

---
+++ react_staging/src/app.js
@@ -0,0 +1,15 @@
+// 创建外壳组件
+
+import React,{Component} from 'react'
+
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
