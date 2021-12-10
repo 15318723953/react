ec15cf539c7df8d6c8e2a03fecf7ba4669e52570
zhaozengxing <961854268@qq.com>
2021-12-10 17:41:16 +0800

    2021/12/10

---
+++ react_staging/src/components/Count/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    state = {
+        count: 0
+    }
+    increment = () => {
+        const {value} = this.selectNumber
+        const {count} = this.state 
+        this.setState({
+            count:count+value
+        })
+    }
+    decrement = () => {
+
+    }
+    incrementIfOdd = () => {
+
+    }
+    incrementAsync = () => {
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>当前求和为:{this.state.count}</h1>
+                <select ref={c => this.selectNumber = c} name="" id="">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数+</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步+</button>&nbsp;
+            </div>
+        );
+    }
+}
+
+export default index;
--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 // 引入react-dom
 import ReactDOM from 'react-dom'
 //引入app页面
-import App from './app'
+import App from './app.jsx'
 
 
 ReactDOM.render(<App />, document.querySelector('#root'))
