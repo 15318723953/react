052970a8d132030312ca2a565fef053a14f98674
zhaozengxing <961854268@qq.com>
2021-12-29 17:58:51 +0800

    2021-12-29

--- react_staging/src/components/5_context/index.jsx
+++ react_staging/src/components/5_context/index.jsx
@@ -10,7 +10,7 @@ class A extends Component {
             <div>
                 <h3>我是A组件</h3>
                 <p>我的用户名是{userName}</p>
-                <Provider value={[userName, age]}>
+                <Provider value={{userName:userName,age:age}}>
                     <B userName={userName} />
                 </Provider>
 
@@ -31,34 +31,34 @@ class B extends Component {
     }
 }
 // 第一种方式只能适用于类式组件
-// class C extends Component {
-//     static contextType = MyContext  // 使用context是需要先声明接收
-//     render() {
-//         return (
-//             <div>
-//                 <h3>我是C组件</h3>
-//                 <p>我从A组件接受到的用户名{this.context}</p>
+class C extends Component {
+    static contextType = MyContext  // 使用context是需要先声明接收
+    render() {
+        return (
+            <div>
+                <h3>我是C组件</h3>
+                <p>我从A组件接受到的用户名{this.context.userName}</p>
 
-//             </div>
-//         );
-//     }
-// }
-// 第二种,类式和函数式都可以使用
-function C() {
-    return (
-        <div>
-            <h3>我是C组件</h3>
-            <p>我从A组件接受到的用户名
-            <Consumer>
-            {
-                value =>{
-                    return value[0]
-                }
-            }
-            </Consumer>
-            </p>
-
-        </div>
-    );
+            </div>
+        );
+    }
 }
+// 第二种,类式和函数式都可以使用
+// function C() {
+//     return (
+//         <div>
+//             <h3>我是C组件</h3>
+//             <p>我从A组件接受到的用户名
+//             <Consumer>
+//             {
+//                 value =>{
+//                     return `${value.userName}我的年龄是${value.age}`
+//                 }
+//             }
+//             </Consumer>
+//             </p>
+
+//         </div>
+//     );
+// }
 export default A;
